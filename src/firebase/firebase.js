5be3b2791d3fb5d857a44abbc1a91e98ec3f2d66
Krishna Bhatt <92638917+ikrishnabhatt@users.noreply.github.com>
2024-10-30 19:52:44 +0530

    Updated the firebase files

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-	apiKey: "//",
-	authDomain: "//",
-  	projectId: "//",
-  	storageBucket: "//",
-  	messagingSenderId: "//",
-  	appId: "//",
-  	measurementId: "//"
+	apiKey: "AIzaSyAetfZIOMP7aEGQ_vGbS9i_epKlCUmJvrg",
+	authDomain: "zorzo-42e5f.firebaseapp.com",
+  	projectId: "zorzo-42e5f",
+  	storageBucket: "zorzo-42e5f.appspot.com",
+  	messagingSenderId: "936276485821",
+  	appId: "1:936276485821:web:3068395a9018503b1e79d4",
+  	measurementId: "G-R5YXXKQRFW"
 };
 
 const app = initializeApp(firebaseConfig);
